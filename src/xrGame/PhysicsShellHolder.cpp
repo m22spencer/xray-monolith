a2b4241a750cd92687f850a5b9ab7821a3df38a6
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2025-04-15 23:36:04 +1000

    Revert "Merge pull request #157 from xr-lucy/lucy_stuff"
    This reverts commit 349ccc802d651259e2ce72b451a29bfea1096c2f.

--- src/xrGame/PhysicsShellHolder.cpp
+++ src/xrGame/PhysicsShellHolder.cpp
@@ -379,12 +379,7 @@ void CPhysicsShellHolder::OnChangeVisual()
 			char_support->destroy_imotion();
 
 		VERIFY(!character_physics_support() || !character_physics_support()->interactive_motion());
-		if (m_pPhysicsShell)
-		{
-			m_pPhysicsShell->set_Kinematics(nullptr);
-			m_pPhysicsShell->Deactivate();
-		}
-
+		if (m_pPhysicsShell)m_pPhysicsShell->Deactivate();
 		xr_delete(m_pPhysicsShell);
 		VERIFY(0==m_pPhysicsShell);
 	}
