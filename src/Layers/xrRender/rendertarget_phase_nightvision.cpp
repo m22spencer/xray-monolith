m22spencer/xray-monolith

commit 9ea55b15742f69686cd3c81b715789de5ed322ad

Add a preprocess stage for scope image buffer

src/Layers/xrRender/rendertarget_phase_nightvision.cpp
@@ -91,11 +91,23 @@ void CRenderTarget::phase_fakescope()
 	pv->set(float(w), 0, d_Z, d_W, C, p1.x, p0.y); pv++;
 	RCache.Vertex.Unlock(4, g_combine->vb_stride);
 
-	//Set pass
-	RCache.set_Element(s_fakescope->E[ps_r2_nightvision]);
-
 	//Set geometry
 	RCache.set_Geometry(g_combine);
+
+	if (Device.m_SecondViewport.IsSVPFrame()) {
+		// For effects like heatvision within scope
+		static ref_shader shader_3dss;
+		shader_3dss.create("scope_preprocess");
+		RCache.set_Element(shader_3dss->E[1]);
+
+		RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
+
+		HW.pContext->CopyResource(rt_secondVP->pTexture->surface_get(), dest_rt->pTexture->surface_get());
+		return;
+	}
+
+	//Set pass
+	RCache.set_Element(s_fakescope->E[ps_r2_nightvision]);
 	RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
 
 	HW.pContext->CopyResource(rt_Generic_0->pTexture->surface_get(), dest_rt->pTexture->surface_get());

gamedata/shaders/r3/scope_preprocess.ps
@@ -1,0 +1,7 @@
+#include "night_vision.h"
+#include "common.h"
+
+float4 main(p_screen I,  float4 pos2d : SV_POSITION) : SV_Target
+{
+	return float4(s_image.Sample(smp_base, I.tc0).rgb, 1.0);
+} 

gamedata/shaders/r3/scope_preprocess.s
@@ -1,0 +1,8 @@
+function normal(shader, t_base, t_second, t_detail)
+	shader:begin("stub_notransform_t", "scope_prepass")
+	: zb(false, false)
+	: blend(true, blend.srcalpha, blend.invsrcalpha)
+	shader:dx10texture("s_image", "$user$generic0")
+	shader:dx10texture("s_position", "$user$generic2")
+	shader:dx10sampler("smp_base")
+end

src/Layers/xrRenderPC_R4/r4_rendertarget_phase_combine.cpp
@@ -481,7 +481,7 @@ void CRenderTarget::phase_combine()
 		if (ps_sunshafts_mode == R2SS_SCREEN_SPACE || ps_sunshafts_mode == R2SS_COMBINE_SUNSHAFTS)
 			phase_sunshafts();
 	}
-	
+		
 	if (scope_3D_fake_enabled)
 	{
 		phase_3DSSReticle(); // Redotix99: for 3D Shader Based Scopes
@@ -523,16 +523,15 @@ void CRenderTarget::phase_combine()
 		phase_heatvision();
 	//--DSR-- HeatVision_end
 
+	if (scope_fake_enabled || Device.m_SecondViewport.IsSVPFrame())
+	{
+		phase_fakescope(); //crookr
+	}
+
 	if (Device.m_SecondViewport.IsSVPFrame()) {
 		// At this point, the scope view is done. 
 		//    we do not want to post process.
-		HW.pContext->CopyResource(rt_secondVP->pSurface, rt_Color->pSurface);
 		return;
-	}
-
-	if (scope_fake_enabled)
-	{
-		phase_fakescope(); //crookr
 	}
 
     //SMAA

commit 314a93b8215b546cb75c749e95e802a38163f9ce

fix regression from 9ea55b15 which caused the game to hang on exit + static variable to rc bad

src/Layers/xrRender/rendertarget_phase_nightvision.cpp
@@ -95,11 +95,7 @@ void CRenderTarget::phase_fakescope()
 	RCache.set_Geometry(g_combine);
 
 	if (Device.m_SecondViewport.IsSVPFrame()) {
-		// For effects like heatvision within scope
-		static ref_shader shader_3dss;
-		if (!shader_3dss)
-			shader_3dss.create("scope_preprocess");
-		RCache.set_Element(shader_3dss->E[1]);
+		RCache.set_Element(s_scope_preprocess->E[1]);
 
 		RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
 

src/Layers/xrRenderPC_R2/r2_rendertarget.cpp
@@ -335,6 +335,7 @@ CRenderTarget::CRenderTarget()
 	s_gasmask_dudv.create(b_gasmask_dudv, "r2\\gasmask_dudv");
 	s_nightvision.create(b_nightvision, "r2\\nightvision");
 	s_fakescope.create(b_fakescope, "r2\\fakescope"); //crookr
+	s_scope_preprocess.create("scope_preprocess");
 	s_heatvision.create(b_heatvision, "r2\\heatvision"); //--DSR-- HeatVision
 	s_lut.create(b_lut, "r2\\lut");
 	// OCCLUSION

src/Layers/xrRenderPC_R2/r2_rendertarget.h
@@ -132,6 +132,7 @@ private:
 	ref_shader s_gasmask_dudv;
 	ref_shader s_nightvision;
 	ref_shader s_fakescope; //crookr
+	ref_shader s_scope_preprocess;
 	ref_shader s_heatvision; //--DSR-- HeatVision
 
 	ref_shader s_blur;

src/Layers/xrRenderPC_R3/r3_rendertarget.cpp
@@ -489,6 +489,7 @@ CRenderTarget::CRenderTarget()
 	s_gasmask_dudv.create(b_gasmask_dudv, "r2\\gasmask_dudv");
 	s_nightvision.create(b_nightvision, "r2\\nightvision");
 	s_fakescope.create(b_fakescope, "r2\\fakescope"); //crookr
+	s_scope_preprocess.create("scope_preprocess");
 	s_heatvision.create(b_heatvision, "r2\\heatvision"); //--DSR-- HeatVision
 	s_lut.create(b_lut, "r2\\lut");
 	// OCCLUSION

src/Layers/xrRenderPC_R3/r3_rendertarget.h
@@ -182,6 +182,7 @@ private:
 	ref_shader s_gasmask_dudv;
 	ref_shader s_nightvision;
 	ref_shader s_fakescope; //crookr
+	ref_shader s_scope_preprocess;
 	ref_shader s_heatvision; //--DSR-- HeatVision
 	ref_shader s_lut;
 

src/Layers/xrRenderPC_R4/r4_rendertarget.cpp
@@ -618,6 +618,7 @@ CRenderTarget::CRenderTarget()
 	s_nightvision.create(b_nightvision, "r2\\nightvision");
 
 	s_fakescope.create(b_fakescope, "r2\\fakescope"); //crookr
+	s_scope_preprocess.create("scope_preprocess");
 
 	s_heatvision.create(b_heatvision, "r2\\heatvision"); //--DSR-- HeatVision
 	s_lut.create(b_lut, "r2\\lut");

src/Layers/xrRenderPC_R4/r4_rendertarget.h
@@ -274,6 +274,7 @@ private:
 	ref_shader s_gasmask_dudv;
 	ref_shader s_nightvision;
 	ref_shader s_fakescope; //crookr
+	ref_shader s_scope_preprocess;
 	ref_shader s_heatvision; //--DSR-- HeatVision
 	ref_shader s_smaa;
 