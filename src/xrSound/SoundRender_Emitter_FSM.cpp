ebec2286c7edd605f93ba5bebda25728bdef90c2
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2025-07-09 20:03:44 +1000

    Gradually increase sound delay with 30m threshold
    (cherry picked from commit ef313c70f88769decf7dd9de9bc4a3573107e5ff)

--- src/xrSound/SoundRender_Emitter_FSM.cpp
+++ src/xrSound/SoundRender_Emitter_FSM.cpp
@@ -50,12 +50,36 @@ void CSoundRender_Emitter::update(float dt)
 	}
 
 	// demonized: add preplay update, calculate delay based on distance and speed of sound
+	// Adaptation for user experience
+	// For distances < 30m, no delay
+	// Then gradually ramp up to delay calculated by math
+	// Starting at 90m, full delay kicks in
 	if (need_preplay_update && m_current_state < stPlaying)
 	{
-		if (_valid(p_source.position) && !p_source.position.similar(Fvector().set(0.f, 0.f, 0.f)))
+		if (_valid(p_source.position) && !p_source.position.similar(Fvector().set(0.f, 0.f, 0.f)) && source() && source()->channels_num() == 1)
 		{
+			//Smooth Ramp Using Hermite(Smootherstep)
+			static auto CalculateSmoothSoundDelay = [](float distance, float speedOfSound, float rampStart, float rampRange)
+			{
+				if (distance <= rampStart)
+					return 0.0f;
+
+				float delay = distance / speedOfSound;
+
+				// Ramp factor from 0 to 1 between rampStart and rampStart + rampRange
+				float t = std::clamp((distance - rampStart) / rampRange, 0.0f, 1.0f);
+
+				// Smootherstep for smooth transition
+				float smoothT = t * t * t * (t * (t * 6 - 15) + 10);
+
+				return delay * smoothT;
+			};
+
 			float speedOfSound = 343.f;
-			auto delay = (p_source.position.distance_to(SoundRender->listener_position()) / speedOfSound) * soundSmoothingParams::distanceBasedDelayPower;
+			float oldDelay = starting_delay;
+			auto oldState = m_current_state;
+			auto delay = CalculateSmoothSoundDelay(p_source.position.distance_to(SoundRender->listener_position()), speedOfSound, 30, 60);
+			delay *= soundSmoothingParams::distanceBasedDelayPower;
 			if (delay > 0.f)
 			{
 				if (m_current_state == stStarting || m_current_state == stStartingLooped)
@@ -68,7 +92,13 @@ void CSoundRender_Emitter::update(float dt)
 					starting_delay += delay;
 				}
 				need_preplay_update = false;
-				Msg("CSoundRender_Emitter::update, file %s, state %s, need_preplay_update, delay %.2f", source()->file_name(), magic_enum::enum_name(m_current_state).data(), starting_delay);
+				/*Msg("CSoundRender_Emitter::update, file %s, state %s, need_preplay_update, distance %.2f, old delay %.2f, delay %.2f",
+					source() && source()->file_name() ? source()->file_name() : "null",
+					magic_enum::enum_name(oldState).data(),
+					p_source.position.distance_to(SoundRender->listener_position()),
+					oldDelay,
+					starting_delay
+				);*/
 			}
 		}
 	}
