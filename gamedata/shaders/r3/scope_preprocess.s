350bb31ff65d7b144ee2c53b19db07907c3db820
Matthew Spencer <m22spencer@gmail.com>
2025-07-26 09:10:43 -0500

    fix scope_preprocess mistypes

--- gamedata/shaders/r3/scope_preprocess.s
+++ gamedata/shaders/r3/scope_preprocess.s
@@ -1,5 +1,5 @@
 function normal(shader, t_base, t_second, t_detail)
-	shader:begin("stub_notransform_t", "scope_prepass")
+	shader:begin("stub_notransform_t", "scope_preprocess")
 	: zb(false, false)
 	: blend(true, blend.srcalpha, blend.invsrcalpha)
 	shader:dx10texture("s_image", "$user$generic0")
