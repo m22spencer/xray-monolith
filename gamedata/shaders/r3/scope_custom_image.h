6fa9dfe2f0231bb5784bbf822bee302caf7ec36d
Matthew Spencer <m22spencer@gmail.com>
2025-07-28 20:25:01 -0500

    Fix scope_custom_image sample shader

--- gamedata/shaders/r3/scope_custom_image.h
+++ gamedata/shaders/r3/scope_custom_image.h
@@ -3,9 +3,6 @@
 // This file is responsible for generating the image you see underneath the reticle
 
 float3 scope_custom_image(Scope s) {
-    if (distance(s.sfp, s.center) >= s.radius)
-        return float3(0, 0, 0);
-
     float2 tc = SCOPECOORD_TO_TEXCOORD(s.sfp);
     return SampleBackbuffer(tc);
 }
