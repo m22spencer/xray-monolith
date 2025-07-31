149149ecc190ff8173c74ca894d79171dbd93d46
Matthew Spencer <m22spencer@gmail.com>
2025-07-31 18:26:34 -0500

    clamp svp_scope value from 0-2 again

--- src/Layers/xrRender/xrRender_console.cpp
+++ src/Layers/xrRender/xrRender_console.cpp
@@ -1303,7 +1303,7 @@ void xrRender_initconsole()
 
 	CMD4(CCC_Integer, "r__fakescope", &scope_fake_enabled, 0, 1); //crookr for fake scope
 	CMD4(CCC_Integer, "r__3Dfakescope", &scope_3D_fake_enabled, 0, 1); // Redotix99: for 3D Shader Based Scopes
-	CMD4(CCC_Integer, "r__svpscope", &scope_svp_enabled, 0, 10);
+	CMD4(CCC_Integer, "r__svpscope", &scope_svp_enabled, 0, 2);
 	CMD4(CCC_Integer, "r__scope_debug", &scope_debug, 0, 4);
 
 	CMD4(CCC_Integer, "r__heatvision", &ps_r2_heatvision, 0, 1); //--DSR-- HeatVision
