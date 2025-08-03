0273ab2a82af6c6263448bf7db993fdaacb5e1e1
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2025-08-04 03:04:25 +1000

    Alternative fire_wound formula by Jurko, to be revised later

--- src/xrGame/ActorHelmet.cpp
+++ src/xrGame/ActorHelmet.cpp
@@ -236,32 +236,76 @@ float CHelmet::HitThroughArmor(float hit_power, s16 element, float ap, bool& add
 	float NewHitPower = hit_power;
 	if (hit_type == ALife::eHitTypeFireWound)
 	{
-		float ba = GetBoneArmor(element);
-		if (ba <= 0.0f)
-			return NewHitPower;
-
-		float BoneArmor = ba * GetCondition();
-		if (ap <= BoneArmor)
+		// demonized: Alternative formula by Jurko, to be revised later
+		if (pSettings->line_exist(cNameSect(), "fire_wound_param_1") && pSettings->line_exist(cNameSect(), "fire_wound_param_2"))
 		{
-			//пуля НЕ пробила бронь
-			NewHitPower *= m_boneProtection->m_fHitFracActor;
-			//add_wound = false; 	//раны нет
-			if (strstr(Core.Params, "-dbgbullet"))
-				Msg("CHelmet::HitThroughArmor AP(%f) <= bone_armor(%f) [HitFracActor=%f] modified hit_power=%f", ap,
-				    BoneArmor, m_boneProtection->m_fHitFracActor, NewHitPower);
-		}
+			float ba = GetBoneArmor(element);
+			if (ba <= 0.0f)
+				return NewHitPower;
 
+			float BoneArmor = ba * GetCondition();
+			float var1 = pSettings->r_float(cNameSect(), "fire_wound_param_1");
+			float var2 = pSettings->r_float(cNameSect(), "fire_wound_param_2");
+			if (ap <= BoneArmor / var2)
+			{
+				float d_hit_power = m_boneProtection->m_fHitFracActor;
+
+				if (ap <= BoneArmor * var2)
+				{
+					d_hit_power = m_boneProtection->m_fHitFracActor * var1;
+				}
+
+				NewHitPower *= d_hit_power;
+
+				if (strstr(Core.Params, "-dbgbullet"))
+					Msg("CHelmet::HitThroughArmor AP(%f) <= bone_armor(%f) [HitFracActor=%f] modified hit_power=%f", ap,
+						BoneArmor, m_boneProtection->m_fHitFracActor, NewHitPower);
+			}
+			else
+			{
+				float d_hit_power = (ap - BoneArmor * var2) / (ap * (1.1f - BoneArmor * var1));
+				if (ap / 2 > BoneArmor / var2)
+				{
+					d_hit_power = d_hit_power / var1;
+				}
+
+				clamp(d_hit_power, m_boneProtection->m_fHitFracActor, 1.0f);
+
+				NewHitPower *= d_hit_power;
+
+				if (strstr(Core.Params, "-dbgbullet"))
+					Msg("CHelmet::HitThroughArmor AP(%f) > bone_armor(%f) [HitFracActor=%f] modified hit_power=%f", ap,
+						BoneArmor, m_boneProtection->m_fHitFracActor, NewHitPower);
+			}
+		}
 		else
 		{
-			float d_hit_power = (ap - BoneArmor) / (ap * m_boneProtection->APScale);
-			clamp(d_hit_power, m_boneProtection->m_fHitFracActor, 1.0f);
+			float ba = GetBoneArmor(element);
+			if (ba <= 0.0f)
+				return NewHitPower;
 
-			NewHitPower *= d_hit_power;
+			float BoneArmor = ba * GetCondition();
+			if (ap <= BoneArmor)
+			{
+				//пуля НЕ пробила бронь
+				NewHitPower *= m_boneProtection->m_fHitFracActor;
+				//add_wound = false; 	//раны нет
+				if (strstr(Core.Params, "-dbgbullet"))
+					Msg("CHelmet::HitThroughArmor AP(%f) <= bone_armor(%f) [HitFracActor=%f] modified hit_power=%f", ap,
+						BoneArmor, m_boneProtection->m_fHitFracActor, NewHitPower);
+			}
+			else
+			{
+				float d_hit_power = (ap - BoneArmor) / (ap * m_boneProtection->APScale);
+				clamp(d_hit_power, m_boneProtection->m_fHitFracActor, 1.0f);
+
+				NewHitPower *= d_hit_power;
+
+				if (strstr(Core.Params, "-dbgbullet"))
+					Msg("CHelmet::HitThroughArmor AP(%f) > bone_armor(%f) [HitFracActor=%f] modified hit_power=%f", ap,
+						BoneArmor, m_boneProtection->m_fHitFracActor, NewHitPower);
+			}
 		}
-
-		if (strstr(Core.Params, "-dbgbullet"))
-			Msg("CHelmet::HitThroughArmor AP(%f) > bone_armor(%f) [HitFracActor=%f] modified hit_power=%f", ap,
-			    BoneArmor, m_boneProtection->m_fHitFracActor, NewHitPower);
 	}
 	else
 	{
